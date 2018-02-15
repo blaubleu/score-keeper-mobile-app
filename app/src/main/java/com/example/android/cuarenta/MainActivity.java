4e9522963931b117b3de29ec288a1d150db8e1a5
Anahi Costa <moanableu@gmail.com>
2018-02-14 19:51:26 -0500

    worked on it a bit. The winning team message pops up for team Chullas not for team viejos. \n We can score a Caida prior to reaching 38 for team Viejos but not Chullas. If I add a way to score the button goes away. \n I left each team w/different logic so that you can see what I was trying

--- app/src/main/java/com/example/android/cuarenta/MainActivity.java
+++ app/src/main/java/com/example/android/cuarenta/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.android.cuarenta;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
@@ -12,13 +13,20 @@ public class MainActivity extends AppCompatActivity {
 
     int scoreChullas = 0;
     int scoreViejos = 0;
-//    boolean onlyWayToFortyC = false;
+//    boolean onlyWayToFortyV = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void winner(){
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+        alertDialogBuilder.setMessage("X Team wins, do you want to play another hand?");
+        AlertDialog alertDialog = alertDialogBuilder.create();
+        alertDialog.show();
+        }
 
         /**
          *   Chullas section starts here
@@ -29,49 +37,30 @@ public class MainActivity extends AppCompatActivity {
         displayChullas(scoreChullas);
     }
 
-        /**
-         *   +2 if you receive 3 cards of matching value
-         */
-
-    public void twoToChullasRonda(View view) {
-        scoreChullas = scoreChullas + 2;
-        displayChullas(scoreChullas);
-        if (scoreChullas >= 39) {
-            caidaToast();
-        }
-    }
-
     /**
-     * +2 if you snap a card that was placed on the table by your opponent
+     *  One method to rule them all
+     *  All the possible ways to score +2 pts for team Chullas
      */
-    public void twoToChullasLimpia(View view) {
+    public void cuarentaPlusChullas(View view){
+        if (scoreChullas >= 38) {
+            caidaToast();
+            return;
+        }
         scoreChullas = scoreChullas + 2;
         displayChullas(scoreChullas);
-        if (scoreChullas >= 39) {
-            caidaToast();
-        }
-    }
-
-    /**
-     * +2 until you reach the number of cards you earned -start counting after 20 cards-
-     */
-    public void pairNumberAfterTwentyCardsInStackChullas(View view) {
-        scoreChullas = scoreChullas + 2;
-        displayChullas(scoreChullas);
-        if (scoreChullas >= 39) {
-            caidaToast();
-        }
     }
 
     /**
      * 38 points won't allow anything but to use method twoToChullasCaida
      */
     public void onlyWayToFortyC(View view) {
-        scoreChullas = scoreChullas + 2;
-        displayChullas(scoreChullas);
-/*        if (scoreChullas == 40) {
-            reset();
-        }*/
+        if (scoreChullas == 38) {
+            scoreChullas = scoreChullas + 2;
+            displayChullas(scoreChullas);
+            //alert goes here
+            winner();
+            return;
+        }
     }
 
     /**
@@ -97,66 +86,39 @@ public class MainActivity extends AppCompatActivity {
     }
 
     /**
-     * Reset method -- totally starting here, does not work :(
-     */
-/*    public void reset() {
-        if ((scoreChullas == 40) && onlyWayToFortyC == true){
-            scoreChullas = 0;
-            displayChullas(scoreChullas);
-        }
-    }*/
-
-    /**
      *    Viejos section starts here
      *    -2 to opponent for bad dealing
      */
     public void twoFromViejosBadDealing(View v) {
         scoreViejos = scoreViejos - 2;
-        displayViejos (scoreViejos);
+        displayViejos(scoreViejos);
     }
 
     /**
-     *  +2 if you receive 3 cards of matching value
+     *  One method to rule them all
+     *  All the possible ways to score +2 pts for team Viejos
      */
-    public void twoToViejosRonda (View v) {
-        scoreViejos = scoreViejos + 2;
-        displayViejos (scoreViejos);
-        if (scoreViejos >= 39) {
+    public void cuarentaPlusViejos(View view){
+        if (scoreViejos >= 38) {
             caidaToast();
+            return;
         }
-    }
-
-    /**
-     *   +2 until you reach the number of cards you earned -start counting after 20 cards-
-     */
-    public void twoToViejosLimpia (View v) {
         scoreViejos = scoreViejos + 2;
-        displayViejos (scoreViejos);
-        if (scoreViejos >= 39) {
-            caidaToast();
-        }
+        displayViejos(scoreViejos);
     }
 
     /**
-     *  +2 until you reach the number of cards counted -start counting after 20 cards-
+     * 38 points won't allow anything but to use method twoToChullasCaida
      */
-    public void pairNumberAfterTwentyCardsInStackViejos (View v) {
+    public void onlyWayToFortyV(View view) {
         scoreViejos = scoreViejos + 2;
-        displayViejos (scoreViejos);
-        if (scoreViejos >= 39) {
-            caidaToast();
-        }
+        displayViejos(scoreViejos);
+            if (scoreViejos == 38) {
+                    //alert goes here
+                    winner();
+                    return;
+                }
     }
-
-    /**
-     *     38 points won't allow anything but to use method twoToViejosCaida
-     *     NOTE: pending update once Chullas method passes test
-     *
-     */
-      public void onlyWayToFortyV(View view) {
-          scoreViejos = scoreViejos + 2;
-          displayViejos (scoreViejos);
-      }
 
     /**
      * Displays the score for Team Viejos
@@ -167,22 +129,12 @@ public class MainActivity extends AppCompatActivity {
     }
 
     /**
-     *  Should be able to reset game at any point but alwasy reset the game one 40 is reached - not tested
+     *  Reset scores
      */
-    // This is the only place you use this code.  As it's attached to a button the point is to reset the scores.
-    // uncomment out some of the code below if that wasn't your intent.
     public void reset (View v) {
          	scoreChullas = 0;
             scoreViejos = 0;
              displayChullas(scoreChullas);
              displayViejos(scoreViejos);
-        //I would like to reset the game in this section , but adding an iff statement as below these lines does not work
-        //but I did notice I need to have the option to reset prior to reaching
-
- /*           if (scoreViejos == 40 || scoreChullas == 40){
-                    scoreChullas = 0;
-                    scoreViejos = 0;
-        }*/
-
     }
 }
